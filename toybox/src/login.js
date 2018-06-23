f6fe8b2b7a803d722df951626afc180b7d985794
Janice Lee <janicelee1120@gmail.com>
2018-06-22 21:08:23 -0400

    basic login stuff

---
+++ toybox/src/login.js
@@ -0,0 +1,7 @@
+// $("#create-account-link").click(function(e){
+//     e.preventDefault();
+//     $(".toggle").hide();
+//     $("#create-account").show();
+// });
+
+alert("hallo ill add stuff later sry i suck");
