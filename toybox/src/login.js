bce510afcc5f426986cd0d6d1cc8e50b1e097760
Janice Lee <janicelee1120@gmail.com>
2018-06-25 23:54:25 -0400

    Tiny click things

--- toybox/src/login.js
+++ toybox/src/login.js
@@ -1,7 +1,15 @@
-// $("#create-account-link").click(function(e){
-//     e.preventDefault();
-//     $(".toggle").hide();
-//     $("#create-account").show();
-// });
+$(document).ready(function() {
+  $("#create-account-link").click(function(e){
+      e.preventDefault();
+      $(".toggle").hide();
+      $("#create-account").show();
+  });
+});
 
-alert("hallo ill add stuff later sry i suck");
+$(document).ready(function() {
+  $("#signin-link").click(function(e){
+      e.preventDefault();
+      $(".toggle").hide();
+      $("#sign-in").show();
+  });
+});
